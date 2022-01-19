0cce3096edafc86f213823b90bd20214110bbcc5
bingbong-dev <jasperhv@stud.ntnu.no>
2022-01-19 22:35:13 +0100

    final update and push

--- CNN_test.py
+++ CNN_test.py
@@ -1,24 +1,15 @@
-###########################################################
-import os
-import sys
 from os.path import isdir, join
 from pathlib import Path
 import pandas as pd
-import contextlib
-import wave
-import collections
-import re
 from shutil import copyfile
-import random
-import files_split
+import gc
 # Math
 import numpy as np
 from scipy.fftpack import fft
 from scipy import signal
 from scipy.io import wavfile
-import librosa
-
 from sklearn.decomposition import PCA
+import os
 
 # Visualization
 import matplotlib.pyplot as plt
@@ -30,7 +21,7 @@ import plotly.offline as py
 import plotly.graph_objs as go
 import plotly.tools as tls
 import pandas as pd
-import train_set_arrange
+
 ###########################################################
 
 from tensorflow import keras
@@ -40,70 +31,74 @@ from keras.layers import Conv2D, MaxPooling2D
 from tensorflow.keras.layers import BatchNormalization
 from keras.layers.advanced_activations import LeakyReLU
 import tensorflow as tf
-import split_file_to_use as mf
+from tensorflow.keras.callbacks import Callback
+from keras.regularizers import l1
 
-epochs = 60
+batch_size = 64
 num_classes = 30
 
 #Neural Network Architecture
 proj_model = tf.keras.Sequential([
   tf.keras.layers.Rescaling(1./255),
-  tf.keras.layers.Conv2D(64, 3, activation='relu'),
+  tf.keras.layers.Conv2D(64, 3, padding='same', activation='relu'),
   tf.keras.layers.MaxPooling2D(),
-  tf.keras.layers.Dropout(0.5),
-  tf.keras.layers.Conv2D(64, 3, activation='relu'),
+  tf.keras.layers.Dropout(0.4),
+  tf.keras.layers.Conv2D(64, 3, padding='same', activation='relu'),
   tf.keras.layers.MaxPooling2D(),
-  tf.keras.layers.Dropout(0.5),
-  tf.keras.layers.Conv2D(64, 3, activation='relu'),
+  tf.keras.layers.Dropout(0.4),
+  tf.keras.layers.Conv2D(128, 3, padding='same', activation='relu'),
   tf.keras.layers.MaxPooling2D(),
-  tf.keras.layers.Dropout(0.5),
+  tf.keras.layers.Dropout(0.4),
   tf.keras.layers.Flatten(),
-  tf.keras.layers.Dense(64, activation='relu'),
-  tf.keras.layers.Dropout(0.5),
+  tf.keras.layers.Dense(128, activation='relu'),
+  tf.keras.layers.Dropout(0.4),
   tf.keras.layers.Dense(num_classes)
 ])
+
+#activity_regularizer=l1(0.001)
 
 # Compile the model
 proj_model.compile(
   optimizer='adam',
   loss=tf.losses.SparseCategoricalCrossentropy(from_logits=True),
   metrics=['accuracy']
-  )
+)
+#proj_model.summary()
 
+
+#Collect data
 training_dataset = tf.keras.utils.image_dataset_from_directory(
-    "../Data/FINISHED_WORD_train",
+    "/content/drive/MyDrive/utveksling/FAG/Malis/Project - Speech recognition/Data/FINISHED_WORD_train",
     labels="inferred",
     label_mode="int",
     class_names=None,
     color_mode="rgb",
-    batch_size=64,
-    image_size=(64, 48),
+    batch_size=batch_size,
+    image_size=(int(640*0.1), int(480*0.1)),
     shuffle=True,
-    seed=25,
+    seed=123,
     validation_split=0.2,
-    subset='training',
+    subset="training",
     interpolation="bilinear",
     follow_links=False,
     crop_to_aspect_ratio=False
 )
-
-validation_dataset = tf.keras.utils.image_dataset_from_directory(
-    "../Data/FINISHED_WORD_train",
+val_dataset = tf.keras.utils.image_dataset_from_directory(
+    "/content/drive/MyDrive/utveksling/FAG/Malis/Project - Speech recognition/Data/FINISHED_WORD_train",
     labels="inferred",
     label_mode="int",
     class_names=None,
     color_mode="rgb",
-    batch_size=64,
-    image_size=(64, 48),
+    batch_size=batch_size,
+    image_size=(int(640*0.1), int(480*0.1)),
     shuffle=True,
-    seed=25,
+    seed=123,
     validation_split=0.2,
-    subset='validation',
+    subset="validation",
     interpolation="bilinear",
     follow_links=False,
     crop_to_aspect_ratio=False
 )
-
 
 """""
 for image_batch, labels_batch in training_dataset:
@@ -114,9 +109,41 @@ for image_batch, labels_batch in training_dataset:
 #Configure the dataset for performance
 AUTOTUNE = tf.data.AUTOTUNE
 training_dataset = training_dataset.cache().prefetch(buffer_size=AUTOTUNE)
+val_dataset = val_dataset.cache().prefetch(buffer_size=AUTOTUNE)
 
-proj_model.fit(
+epochs = 100
+
+#Train of the model
+CNN = proj_model.fit(
   training_dataset,
-  validation_data=validation_dataset,
-  epochs=epochs
+  verbose=1,
+  validation_data=val_dataset,
+  epochs = epochs
 )
+
+
+#Visualizing the data
+acc = CNN.history['accuracy']
+val_acc = CNN.history['val_accuracy']
+
+loss = CNN.history['loss']
+val_loss = CNN.history['val_loss']
+
+epochs_range = range(epochs)
+
+plt.figure(figsize=(8, 8))
+plt.subplot(1, 2, 1)
+plt.plot(epochs_range, acc, label='Training Accuracy')
+plt.plot(epochs_range, val_acc, label='Validation Accuracy')
+ax = plt.gca()
+ax.set_ylim([0, 1])
+plt.legend(loc='lower right')
+plt.title('Training and Validation Accuracy')
+
+plt.subplot(1, 2, 2)
+plt.plot(epochs_range, loss, label='Training Loss')
+plt.plot(epochs_range, val_loss, label='Validation Loss')
+plt.legend(loc='upper right')
+plt.title('Training and Validation Loss')
+plt.show()
+plt.savefig('img3')
